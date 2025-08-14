4dab7644c92f63bcfe4eece71a3e61b05269822f
Priyanjal Saxena <priyanjalsaxena1234@gmail.com>
2025-08-14 13:15:58 +0530

    Added the flatten array function

--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Flatten Array 
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function flattenArray(arr) {
+    if(Array.isArray(arr)) {
+        return arr.reduce((flat,item) => {
+            let newArr = flat.concat(Array.isArray(item) ? flattenArray(item) : item)
+            return newArr
+        },[])
+    }
+}
 
-setupCounter(document.querySelector('#counter'))
+let output = flattenArray([1,[2,[3,4]]])
+
+console.log(output)
